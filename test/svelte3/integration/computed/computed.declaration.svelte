0b25055b83cebddbf1c6d83b1ea252a1ddd5027b
Alexey Mulyukin <alexey.mulyukin@arcadia.spb.ru>
2019-05-14 18:27:42 +0300

    Implement computed properties parsing.

---
+++ test/svelte3/integration/computed/computed.declaration.svelte
@@ -0,0 +1,11 @@
+<script>
+    let width = 1;
+    let height = 2;
+
+    let area;
+    /**
+     * The area of rectangle.
+     * @type {number}
+     */
+    $: area = width * height;
+</script>
---
+++ test/svelte3/integration/computed/computed.expression.svelte
@@ -0,0 +1,10 @@
+<script>
+    let width = 1;
+    let height = 2;
+
+    /**
+     * The area of rectangle.
+     * @type {number}
+     */
+    $: area = width * height;
+</script>

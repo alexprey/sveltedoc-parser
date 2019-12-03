867de664b62992f7f6a3cfa1ce95e9c06ca8a29c
hontas <lundin.pontus@gmail.com>
2019-12-03 21:05:19 +0100

    add level 0-check and test for global comment

---
+++ test/svelte3/integration/globalComment/globalComment.nested.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let identifier = 1;
+
+  function topLevelFunction() {
+    /**
+     * The component description
+     * @component ComponentName
+     * @example
+     * <ComponentName identifier={identifier} />
+     */
+
+    innerFunction();
+  }
+
+  topLevelFunction();
+</script>
+
+<style>
+
+</style>
+
+<div>
+  <h1>Header {identifier}</h1>
+</div>

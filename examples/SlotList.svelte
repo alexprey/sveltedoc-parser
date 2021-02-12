58238dba243ed537c5221456fd7a51a42ff4ec49
Alexey Mulyukin (alexprey) <alexprey@yandex.ru>
2021-02-12 19:02:16 +0300

    Add slot parameter example

---
+++ examples/SlotList.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let items = [];
+</script>
+
+<div>
+    {#each items as item}
+        <slot prop={item}></slot>
+    {:else}
+        <slot name="empty"></slot>
+    {/each}
+</div>

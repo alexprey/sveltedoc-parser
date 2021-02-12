46323c4162bd4cb5851a479bfea77f26ac0e07b0
Alexey Mulyukin (alexprey) <alexprey@yandex.ru>
2021-02-12 19:23:20 +0300

    Add test for issue #58

--- test/unit/helpers/helpers.spec.js
+++ test/unit/helpers/helpers.spec.js
@@ -121,5 +121,47 @@ let variable = 2;
             expect(block.content).is.eq('\nlet variable = 2;\n');
             expect(block.attributes).is.empty;
         });
+
+        // TODO Re-enable for following issue: https://github.com/alexprey/sveltedoc-parser/issues/58
+        xit('When html block contains complex markup with escaping and string usages should provide a correct result', () => {
+            const content = `
+<script>
+export let items = [];
+</script>
+<Panel title="Basic tabs" shadow={true}>
+  <Tabs bind:items={items}/>
+  <br><br>
+  <strong>Active tab:</strong> {activeTab1[0].title}
+  <br>
+  <br>
+
+  <Code>
+    {@html highlight(
+      '<script> \n' +
+      '   const items = [ \n' + 
+      '     { title: "Tab 1", active: true }, \n' +
+      '     { title: "Tab 2", active: false }, \n' +
+      '     { title: "Tab 3", active: false }, \n' +
+      '     { title: "Tab 4", active: false }, \n' +
+      '   ]; \n' +
+      '</script> \n\n' +
+      '<Tabs bind:items={items} />')}
+  </Code>
+</Panel>
+            `;
+
+            const result = helpers.extractAllHtmlBlocks(content, 'script');
+
+            expect(result).is.exist;
+            expect(result.blocks).is.exist;
+            expect(result.blocks.length).is.eq(1);
+
+            const block = result.blocks[0];
+
+            expect(block).is.exist;
+            expect(block.offset).is.eq(9);
+            expect(block.content).is.eq('\nexport let items = [];\n');
+            expect(block.attributes).is.empty;
+        });
     });
 });

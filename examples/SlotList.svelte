177b9e2d96e82ae983ad09ceb30d81f732e967c3
Alexey Mulyukin (alexprey) <alexprey@yandex.ru>
2021-02-13 18:44:11 +0300

    Update example components

--- examples/SlotList.svelte
+++ examples/SlotList.svelte
@@ -4,6 +4,10 @@
 
 <div>
     {#each items as item}
+        <!-- 
+            Render the one item markup based on data passed in slot property.
+            @param {GenericListItem} prop The item of the list that should be rendered.
+         -->
         <slot prop={item}></slot>
     {:else}
         <slot name="empty"></slot>

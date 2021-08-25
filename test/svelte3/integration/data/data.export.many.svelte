87ccf877b9190035982b2f9c5c5fec2b64a81ab2
ekhaled <me.khaled@gmail.com>
2021-08-25 17:15:19 +0100

    add failing test

--- test/svelte3/integration/data/data.export.many.svelte
+++ test/svelte3/integration/data/data.export.many.svelte
@@ -1,7 +1,10 @@
 <script>
     /* The `a` variable description */
     let a = 1;
+    /* The `b` variable description */
     let b = 'Test';
+    /* The `c` variable description */
+    let c = 'TestC';
 
-    export { a, b };
+    export { a, b, c };
 </script>

12b8144b3e6c004879c57f115957814530755b17
ekhaled <me.khaled@gmail.com>
2021-08-27 17:39:46 +0100

    add failing test for method argument inference

---
+++ test/svelte3/integration/methods/method.typeInference.svelte
@@ -0,0 +1,19 @@
+<script>
+    /**
+     * The method comment.
+     * @param {string} param1 - the first parameter
+     * @param {boolean} [param2=false] - the second parameter
+     * @param param3 - the third parameter of type number
+     * @returns {number} - return value description
+     */
+    export function publicMethod(
+        param1, //this should use jsdoc from above
+        param2, //this should use jsdoc from above
+        param3 = 1, //type and default should be inferred, description from jsdoc
+        param4 = [], //type should be inferred, no description
+        param5 = {}, //type should be inferred, no description
+        param6 = true //type and default should be inferred, no description
+    ) {
+        return 0;
+    };
+</script>

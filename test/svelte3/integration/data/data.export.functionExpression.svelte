5d7ea9b3fa19948f112b740c92e2af96a22feb13
ekhaled <me.khaled@gmail.com>
2021-08-31 13:16:54 +0100

    add failing test for function expression parsing

---
+++ test/svelte3/integration/data/data.export.functionExpression.svelte
@@ -0,0 +1,25 @@
+<script>
+    /* Adds two numbers `a` and `b`*/
+    export let add = function(a = 0, b = 0){
+        return a + b
+    },
+    /**
+     * Subtracts two numbers `b` from `a`
+     * @param {number} a - first number
+     * @param {number} [b=0] - second number
+     * @returns {number} - the difference
+     */
+    subtract = function(a, b){
+        return a - b;
+    },
+    /**
+     * Multiplies two numbers `a` and `b`
+     * @param a - first number
+     * @param b - second number
+     * @returns {number} - the total
+     */
+    multiply = (a = 0, b = 1) => {
+        return a * b
+    };
+
+</script>

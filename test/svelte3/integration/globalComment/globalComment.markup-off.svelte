681f63dd26665fb58dd9c683f28008c252443ae5
j3rem1e <jeremie.brebec@gmail.com>
2023-09-02 11:16:50 +0200

    Add parsing of description after the @component

---
+++ test/svelte3/integration/globalComment/globalComment.markup-off.svelte
@@ -0,0 +1,23 @@
+<script>
+  /** What should we call the user? */
+  export let name = 'world';
+</script>
+
+<!--
+@component
+Here's some documentation for this component.
+It will show up on hover.
+
+- You can use markdown here.
+- You can also use code blocks here.
+- Usage:
+  ```tsx
+  <main name="Arethra">
+  ```
+-->
+<main>
+  <h1>
+    Hello, {name}
+  </h1>
+</main>
+
